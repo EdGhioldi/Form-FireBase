cc8c54663882852b68f7590a2e8f8e5ce8c3170e
Eduardo Ghioldi <ghioldigahonaeduardo@gmail.com>
2024-01-27 17:28:02 -0300

    App Terminada

--- AppClima/script.js
+++ AppClima/script.js
@@ -3,8 +3,8 @@ let api_key = '376fa21a88c2568f03652cd696df9533';
 let difKelvin = 273.15
 
 
-document.getElementById('botonBusqueda').addEventListener(click, () => {
-    const ciudad = getElementById('ciudadEntreada').value;
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value;
     if(ciudad){
         fetchDatosClima(ciudad)
     }
@@ -12,7 +12,40 @@ document.getElementById('botonBusqueda').addEventListener(click, () => {
 
 function fetchDatosClima(ciudad){
     fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
-    .then(response => response.json())
-    .then(response => console.log(response))
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
+}
+
+function mostrarDatosClima(data){
+    const divDatosClima = document.getElementById('datosClima');
+    divDatosClima.innerHTML = '';
+
+    const ciudadNombre = data.name
+    const ciudadPais = data.sys.country
+    const temperatura = data.main.temp
+    const humedad = data.main.humidity
+    const descripcion = data.weather[0].description
+    const icono = data.weather[0].icon
+
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = `${ciudadNombre}, ${ciudadPais}`
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `Temperature is:  ${Math.floor(temperatura - difKelvin)}°C`
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `Humidity is:  ${humedad}%`
+
+    const iconoInfo = document.createElement('img')
+    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `The weather is:  ${descripcion}`
+
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(iconoInfo)
+    divDatosClima.appendChild(descripcionInfo)
 }
 
